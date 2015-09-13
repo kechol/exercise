e9ed476a9a55c6da77edc3222758d1734b15f07d
Kazuyuki SUZUKI <kechol28@gmail.com>
2015-09-13 23:56:50 +0900

    fix atcoder/arc001_3 for invalid input

--- atcoder/arc001_3/a.py
+++ atcoder/arc001_3/a.py
@@ -1,69 +1,60 @@
 #!/usr/bin/env python
 
-from copy import deepcopy
 
-cs = [[]*8]*8
-qs = []
+class TwistedQs:
 
+    def __init__(self):
+        self.map = [['.' for i in range(8)] for j in range(8)]
 
-def fill(cs, q):
-    for i in range(8):
-        cs[q[0]][i] = 'X'
-        cs[i][q[1]] = 'X'
+        # init first Qs
+        for i in range(8):
+            ipt = input()
+            for j in range(8):
+                if ipt[j] == 'Q':
+                    if self.search(i, j):
+                        self.map[i][ipt.index('Q')] = 'Q'
+                    else:
+                        print('No Answer')
+                        exit()
 
-        p = q[0] + q[1] - i
-        if p > -1 and p < 8:
-            cs[i][p] = 'X'
+        if self.setQ(0):
+            print("\n".join(["".join(cx) for cx in self.map]))
+        else:
+            print('No Answer')
 
-        p = q[0] - q[1] + i
-        if p > -1 and p < 8:
-            cs[p][i] = 'X'
+    def setQ(self, i):
+        if i > 7:
+            return True
 
-    cs[q[0]][q[1]] = 'Q'
+        if 'Q' in self.map[i]:
+            return self.setQ(i+1)
 
+        for j in range(8):
+            if self.search(i, j):
+                self.map[i][j] = 'Q'
+                if self.setQ(i+1):
+                    return True
+                self.map[i][j] = '.'
 
-def check(cs):
-    for i in range(8):
-        if '.' not in cs[i] and 'Q' not in cs[i]:
-            return False
-    return True
+        return False
 
+    def search(self, y, x):
+        for i in range(8):
+            if self.map[i][x] == 'Q':
+                return False
 
-def loop(i, cs):
-    if i == 8 and len(qs) == 8:
-        print("\n".join(["".join(c) for c in cs]))
-        exit()
+            if self.map[y][i] == 'Q':
+                return False
 
-    if 'Q' in cs[i]:
-        loop(i+1, cs)
+            p = x + y - i
+            if p > -1 and p < 8 and self.map[i][p] == 'Q':
+                return False
 
-    cst = deepcopy(cs)
+            p = y - x + i
+            if p > -1 and p < 8 and self.map[p][i] == 'Q':
+                return False
 
-    for j in range(8):
-        if cs[i][j] == '.':
-            fill(cs, [i, j])
-            if check(cs):
-                # add Q to map
-                qs.append([i, j])
-                loop(i+1, cs)
-            else:
-                # revert map
-                cs = cst
-
-
-# init map
-for i in range(8):
-    cs[i] = list(input())
-    if 'Q' in cs[i]:
-        q = [i, cs[i].index('Q')]
-        qs.append(q)
+        return True
 
 
-# fill map for three of first Qs
-for q in qs:
-    fill(cs, q)
-
-
-# join the loop
-loop(0, cs)
-print("No Answer")
+TwistedQs()

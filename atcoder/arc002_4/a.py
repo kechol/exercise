056965c8900dfd6bd69458a7e5abf734ad64a1b6
Kazuyuki SUZUKI <kechol28@gmail.com>
2015-09-17 13:53:10 +0900

    halfway to solve atcoder/arc002_4

---
+++ atcoder/arc002_4/a.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+
+class BoardGame:
+
+    def __init__(self, h, w, b):
+        self.h = h
+        self.w = w
+        self.board = b
+
+    def judge(self):
+        while self.move('o', 1):
+            if not self.move('x', -1):
+                return True
+
+        return False
+
+    def move(self, p, d):
+        # print("\n".join(["".join(self.board[i]) for i in range(self.h)]), "\n")
+
+        dist = self.w
+        coor = [0, 0]
+        movable = False
+        for i in range(self.h):
+            if self.board[i][0] == 'x' or self.board[i][w-1] == 'o':
+                return False
+
+            rng = range(0, self.w+d) if (d < 0) else range(d, self.w)
+            for j in rng:
+                if self.board[i][j] == p:
+                    tdist = j if d < 0 else self.w - j - 1
+                    if self.board[i][j+d] == '.' and self.board[i][j+d*2] in ['.', p] and dist > tdist:
+                        movable = True
+                        coor = [i, j]
+                        dist = tdist
+
+        if movable:
+            print(p, coor)
+            self.board[coor[0]][coor[1]] = '.'
+            self.board[coor[0]][coor[1]+d] = p
+
+        return movable
+
+
+if __name__ == '__main__':
+    ipt = input().split(' ')
+    h = int(ipt[0])
+    w = int(ipt[1])
+    b = []
+    for i in range(h):
+        b.append(list(input()))
+
+    bg = BoardGame(h, w, b)
+    if bg.judge():
+        print('o')
+    else:
+        print('x')
